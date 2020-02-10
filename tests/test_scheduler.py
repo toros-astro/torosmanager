d0963bf8d4e1f7f59c5410b1ebd0f72197748f64
Martin Beroiz <martinberoiz@gmail.com>
2020-02-10 14:41:25 -0600

    Added blank tests

---
+++ tests/test_scheduler.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestPreprocessor(unittest.TestCase):
+    def setUp(self):
+        ...
+
+    def tearDown(self):
+        ...
+
+    def test_01(self):
+        raise NotImplementedError
+
+
+if __name__ == "__main__":
+    unittest.main()
