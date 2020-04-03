986cffcfb3b8c548636b483407c836c0b3150afb
Martin Beroiz <martinberoiz@gmail.com>
2020-04-03 16:22:51 -0500

    Minor fixes and bumped to alpha 2

--- torosmanager/__init__.py
+++ torosmanager/__init__.py
@@ -1,12 +1,14 @@
-__version__ = "0.1a1"
+__version__ = "0.1a2"
 
 from . import preprocessor
 from . import config
 from . import models
+import logging
 
 config.init_logger()
-import logging as _logging
-
-_logging.getLogger("init").info("TOROS Manager Started.")
-
-config.init_database()
+logger = logging.getLogger("init")
+logger.info("TOROS Manager Started.")
+try:
+    config.init_database()
+except:
+    logger.exception("Error initiating database.")
